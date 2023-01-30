860c937f3e5b9c084442b802194566532f2cf7ac
Aymen <elhamzaouiaymen@gmail.com>
2023-01-30 16:28:38 +0800

    fix: minor fix

--- components/TodoList.tsx
+++ components/TodoList.tsx
@@ -36,32 +36,28 @@ const TodoList: React.FC<{ tasks: Array<Task> }> = ({ tasks }) => {
       </Grid>
       <Grid item xs={3}>
         <TaskColumn title="In progress">
-          {inProgressTasks.map(
-            ({ title, assigneeProfilePicture, description, status }) => (
-              <TaskCard
-                key={title}
-                title={title}
-                assigneeProfilePicture={assigneeProfilePicture}
-                description={description}
-                status={status}
-              />
-            ),
-          )}
+          {inProgressTasks.map(({ title, assignee, description, status }) => (
+            <TaskCard
+              key={title}
+              title={title}
+              assignee={assignee}
+              description={description}
+              status={status}
+            />
+          ))}
         </TaskColumn>
       </Grid>
       <Grid item xs={3}>
         <TaskColumn title="Done">
-          {doneTasks.map(
-            ({ title, assigneeProfilePicture, description, status }) => (
-              <TaskCard
-                key={title}
-                title={title}
-                assigneeProfilePicture={assigneeProfilePicture}
-                description={description}
-                status={status}
-              />
-            ),
-          )}
+          {doneTasks.map(({ title, assignee, description, status }) => (
+            <TaskCard
+              key={title}
+              title={title}
+              assignee={assignee}
+              description={description}
+              status={status}
+            />
+          ))}
         </TaskColumn>
       </Grid>
     </Grid>
