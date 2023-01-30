699bdda05cbc05d6917c033fd99be63a6e5321eb
Aymen <elhamzaouiaymen@gmail.com>
2023-01-30 12:18:29 +0800

    remove unused import

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,7 +1,7 @@
 import React, { ReactNode } from 'react'
 import Link from 'next/link'
 import Head from 'next/head'
-import { Button, createTheme, ThemeProvider } from '@mui/material';
+import { createTheme, ThemeProvider } from '@mui/material';
 
 type Props = {
   children?: ReactNode
