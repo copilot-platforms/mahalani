61fcd5ff634c9b10ba928457fd843b7980a3b9f0
Neil Raina <mrneilraina@gmail.com>
2023-01-30 17:32:16 +0800

    update status for cards

--- components/TaskCard.tsx
+++ components/TaskCard.tsx
@@ -5,34 +5,77 @@ import {
   CardContent,
   CardHeader,
   Chip,
+  FormControl,
+  InputLabel,
+  Select,
   Stack,
+  MenuItem,
+  SelectChangeEvent,
 } from '@mui/material';
 import { Task, TaskStatus } from './types';
+import { AirtableContext } from '../utils/airtableContext';
+import { getAirtableClient, updateRecord } from '../utils/airtableUtils';
 
-type TaskCardProps = Task;
+interface TaskCardProps extends Task {
+  onStatusChange: (status: TaskStatus) => void;
+}
+
 /**
  * This component represents the task card.
  * @param title The title of the task
  * @param assigneeProfilePicture The profile picture of the assignee
  */
-const TaskCard = ({ title, description, assignee }: TaskCardProps) => (
-  <Card variant="outlined">
-    <CardHeader title={title} />
-    <CardContent>
-      <p>{description}</p>
+const TaskCard = ({ title, assignee, status, id, onStatusChange }: TaskCardProps) => {
+  const appSetupData = React.useContext(AirtableContext);
 
-      <Stack>
-        <div style={{ display: 'flex', alignItems: 'center', gap: '8px' }}>
-          <Avatar
-            sx={{ width: 24, height: 24 }}
-            alt="Remy Sharp"
-            src="/static/images/avatar/1.jpg"
-          />
-          {assignee.givenName} {assignee.familyName}
-        </div>
-      </Stack>
-    </CardContent>
-  </Card>
-);
+  const handleStatusChange = async (event: SelectChangeEvent) => {
+    
+    onStatusChange(event.target.value as TaskStatus);
+
+    const baseConstructor = getAirtableClient(
+      appSetupData.apiKey,
+      appSetupData.baseId,
+    );
+    const tableClient = baseConstructor(appSetupData.tableId);
+
+    try {
+     await updateRecord(tableClient, id, { Status: event.target.value as TaskStatus })
+    } catch (ex) {
+      console.error('Error updating record', ex);
+    }
+  }
+  
+  return (
+    <Card variant="outlined">
+      <CardHeader title={title} />
+      <CardContent>
+        <Stack spacing={2}>
+          <div style={{ display: 'flex', alignItems: 'center', gap: '8px' }}>
+            <Avatar
+              sx={{ width: 24, height: 24 }}
+              alt="Remy Sharp"
+              src="/static/images/avatar/1.jpg"
+            />
+            {assignee.givenName} {assignee.familyName}
+          </div>
+          <FormControl>
+          <InputLabel id={`status-label-${id}`}>Status</InputLabel>
+          <Select
+            labelId={`status-label-${id}`}
+            id={`status-select-${id}`}
+            value={status}
+            label="Status"
+            onChange={handleStatusChange}
+          >
+            <MenuItem value={TaskStatus.Todo}>{TaskStatus.Todo}</MenuItem>
+            <MenuItem value={TaskStatus.InProgress}>{TaskStatus.InProgress}</MenuItem>
+            <MenuItem value={TaskStatus.Done}>{TaskStatus.Done}</MenuItem>
+          </Select>
+        </FormControl>
+        </Stack>
+      </CardContent>
+    </Card>
+  );
+};
 
 export default TaskCard;
