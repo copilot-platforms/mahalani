450ffe97f48d7e16293922070ff7ecd19b18cc24
Neil Raina <mrneilraina@gmail.com>
2023-01-30 12:46:53 +0800

    add airtable config component

--- pages/[appId].tsx
+++ pages/[appId].tsx
@@ -1,19 +1,42 @@
-import Link from 'next/link'
 import { useRouter } from 'next/router'
+import { useState, useEffect } from 'react';
+import AppSetup from '../components/AppSetup';
 import Layout from '../components/Layout'
 
+type SetupCompleteResult = {
+    apiKey: string;
+    baseId: string;
+    tableId: string;
+}
+
+/**
+ * This is the app page container where we can render the configuration
+ * page or the app itself (which gets some client information).
+ * @returns 
+ */
 const AppPage = () => {
     const router = useRouter()
-  const { appId } = router.query
+    const { appId } = router.query
+    const [appSetupData, setAppSetupData] = useState<SetupCompleteResult | null>(null);
+
+    useEffect(() => {
+        const setupData = window.localStorage.getItem(`setupData.${appId}`);
+        if (setupData) {
+            setAppSetupData(JSON.parse(setupData));
+        }
+    }, [appId]);
+
+    const handleSetupComplete = (result: SetupCompleteResult) => {
+        window.localStorage.setItem(`setupData.${appId}`, JSON.stringify(result))
+        setAppSetupData(result);
+    }
     
     return (
-  <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello App: {appId} 👋</h1>
-    <p>
-      <Link href="/about">About</Link>
-    </p>
-  </Layout>
-);
+        <Layout title="Home | Next.js + TypeScript Example">
+            {appSetupData && <div>This app is setup</div>}
+            {!appSetupData && <AppSetup onSetupComplete={handleSetupComplete} /> }
+        </Layout>
+    );
 }
 
 export default AppPage
