c81168e741f372b7821a1af1bfddbecc01d1c352
Aymen <elhamzaouiaymen@gmail.com>
2023-01-30 14:59:27 +0800

    add a link button that takes to todo list page

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,12 +1,12 @@
-import React, { ReactNode } from 'react'
-import Link from 'next/link'
-import Head from 'next/head'
+import React, { ReactNode } from 'react';
+import Link from 'next/link';
+import Head from 'next/head';
 import { createTheme, ThemeProvider } from '@mui/material';
 
 type Props = {
-  children?: ReactNode
-  title?: string
-}
+  children?: ReactNode;
+  title?: string;
+};
 
 const theme = createTheme({
   palette: {
@@ -20,25 +20,23 @@ const theme = createTheme({
 });
 
 const Layout = ({ children, title = 'This is the default title' }: Props) => (
-    <ThemeProvider theme={theme}>
-      <Head>
-        <title>{title}</title>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-      </Head>
-      <header>
-        <nav>
-          <Link href="/">Home</Link> | <Link href="/about">About</Link> |{' '}
-          <Link href="/users">Users List</Link> |{' '}
-          <a href="/api/users">Users API</a>
-        </nav>
-      </header>
-      {children}
-      <footer>
-        <hr />
-        <span>I'm here to stay (Footer)</span>
-      </footer>
+  <ThemeProvider theme={theme}>
+    <Head>
+      <title>{title}</title>
+      <meta charSet="utf-8" />
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+    </Head>
+    <header>
+      <nav>
+        <Link href="/">Home</Link> | <Link href="/todo-list">Todos</Link>
+      </nav>
+    </header>
+    {children}
+    <footer>
+      <hr />
+      <span>I'm here to stay (Footer)</span>
+    </footer>
   </ThemeProvider>
-)
+);
 
-export default Layout
+export default Layout;
