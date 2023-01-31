f92c9f25681391cf02ec0b002f029f32ee25b286
Aymen Hamzaoui <elhamzaouiaymen@gmail.com>
2023-01-31 10:37:04 +0800

    Add drag & drop support to the todo list (#9)
    * feat: add drag and drop behaviour to change tasks status
    
    * move call update airtable api to update task to parent component
    
    * fix: task card ref

--- components/TodoList.tsx
+++ components/TodoList.tsx
@@ -3,10 +3,14 @@ import Layout from './Layout';
 import TaskCard from './TaskCard';
 import TaskColumn from './TaskColumn';
 import { Task, TaskStatus } from './types';
-import { useEffect, useMemo, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
+import { DndProvider } from 'react-dnd';
+import { HTML5Backend } from 'react-dnd-html5-backend';
+import { AirtableContext } from '../utils/airtableContext';
+import { getAirtableClient, updateRecord } from '../utils/airtableUtils';
 
 const TaskStatuses = [TaskStatus.Todo, TaskStatus.InProgress, TaskStatus.Done];
-
+type DroppedTaskCardData = { taskId: string };
 const initialTasksByStatus: Record<TaskStatus, Array<Task>> = {
   [TaskStatus.Todo]: [],
   [TaskStatus.InProgress]: [],
@@ -14,7 +18,15 @@ const initialTasksByStatus: Record<TaskStatus, Array<Task>> = {
 };
 
 const TodoList: React.FC<{ tasks: Array<Task> }> = ({ tasks }) => {
-  const [tasksByStatus, setTasksByStatus] = useState<Record<TaskStatus, Array<Task>>>(initialTasksByStatus);
+  const appSetupData = useContext(AirtableContext);
+  // Get the airtable rest client instance
+  const airtableClient = getAirtableClient(
+    appSetupData.apiKey,
+    appSetupData.baseId,
+  );
+
+  const [tasksByStatus, setTasksByStatus] =
+    useState<Record<TaskStatus, Array<Task>>>(initialTasksByStatus);
 
   /**
    * This is a useEffect that filters the tasks by status.
@@ -34,9 +46,13 @@ const TodoList: React.FC<{ tasks: Array<Task> }> = ({ tasks }) => {
    * @param id task id
    * @param existingStatus existing status
    * @param newStatus new status
-   * @returns 
+   * @returns
    */
-  const handleStatusChanged = (id: string, existingStatus: TaskStatus, newStatus: TaskStatus) => {
+  const handleStatusChanged = async (
+    id: string,
+    existingStatus: TaskStatus,
+    newStatus: TaskStatus,
+  ) => {
     const existingTasks = tasksByStatus[existingStatus];
     const newTasks = tasksByStatus[newStatus];
     const taskToMove = existingTasks.find((task) => task.id === id);
@@ -52,28 +68,63 @@ const TodoList: React.FC<{ tasks: Array<Task> }> = ({ tasks }) => {
     };
 
     setTasksByStatus(updatedTasks);
+
+    const tableClient = airtableClient(appSetupData.tableId);
+
+    try {
+      await updateRecord(tableClient, id, {
+        Status: newStatus,
+      });
+    } catch (ex) {
+      console.error('Error updating record', ex);
+    }
+  };
+
+  /**
+   * When a task card is dropped, call handleStatusChanged to
+   * update the status of the task.
+   * @param param0
+   * @param newTaskStatus
+   */
+  const handleDropTaskCard = (
+    { taskId }: DroppedTaskCardData,
+    newTaskStatus: TaskStatus,
+  ) => {
+    const existingStatus = Object.keys(tasksByStatus).find((key) =>
+      tasksByStatus[key as TaskStatus].find((task) => task.id === taskId),
+    ) as TaskStatus;
+    handleStatusChanged(taskId, existingStatus, newTaskStatus);
   };
 
   return (
-    <Grid container spacing={2}>
-      {Object.entries(tasksByStatus).map(([status, tasks]) => (
-        <Grid item xs={4}>
-        <TaskColumn title={status}>
-          {tasks.map(({ title, assignee, description, status, id }) => (
-            <TaskCard
-              key={title}
-              title={title}
-              assignee={assignee}
-              description={description}
-              status={status}
-              id={id}
-              onStatusChange={(newStatus: TaskStatus) => handleStatusChanged(id, status, newStatus)}
-            />
-          ))}
-        </TaskColumn>
+    <DndProvider backend={HTML5Backend}>
+      <Grid container gap={1} spacing={0}>
+        {Object.entries(tasksByStatus).map(([status, tasks]) => (
+          <Grid item xs={3} key={status}>
+            <TaskColumn
+              title={status}
+              onDrop={(item) => {
+                handleDropTaskCard(item, status as TaskStatus);
+              }}
+            >
+              {tasks.map(({ title, assignee, description, status, id }) => (
+                <TaskCard
+                  key={title}
+                  title={title}
+                  assignee={assignee}
+                  description={description}
+                  status={status}
+                  id={id}
+                  onStatusChange={(newStatus: TaskStatus) =>
+                    handleStatusChanged(id, status, newStatus)
+                  }
+                />
+              ))}
+            </TaskColumn>
+          </Grid>
+        ))}
       </Grid>
-      ))}
-    </Grid>
+    </DndProvider>
   );
 };
 
