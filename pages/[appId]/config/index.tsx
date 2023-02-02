e4fb46a2d1764e8dac9160c2c492a6a3d7a0b3d5
Potluck Mittal <potluck@copilot.com>
2023-02-02 10:59:12 +0800

    show individual URLs per client

--- pages/[appId]/config/index.tsx
+++ pages/[appId]/config/index.tsx
@@ -3,12 +3,15 @@ import { useRouter } from 'next/router';
 import { useState, useEffect } from 'react';
 import AppSetup from '../../../components/AppSetup';
 import Layout from '../../../components/Layout';
+import DataTable from '../../../components/DataTable';
 import { AppContext, AppContextType } from '../../../utils/appContext';
 import { authOptions } from '../../api/auth/[...nextauth]';
 import { fetchConfig } from '../../api/config/apiConfigUtils';
 
+
 type AppSetupPageProps = {
   appConfig: AppContextType | null;
+  clients: any[];
 }
 
 /**
@@ -16,7 +19,7 @@ type AppSetupPageProps = {
  * page or the app itself (which gets some client information).
  * @returns
  */
-const AppSetupPage = ({ appConfig }: AppSetupPageProps) => {
+const AppSetupPage = ({ appConfig, clients }: AppSetupPageProps) => {
   const router = useRouter();
   const { appId } = router.query;
   const [appSetupData, setAppSetupData] = useState<AppContextType | null>(
@@ -37,6 +40,12 @@ const AppSetupPage = ({ appConfig }: AppSetupPageProps) => {
     setAppSetupData(result);
   };
 
+  const myRows = clients.map((client) => ({
+    id: client.id,
+    clientName: `${client.givenName} ${client.familyName}`,
+    url: `https://mahalani.vercel.app/${appId}?clientId=${client.id}`
+  }));
+
   return (
     <AppContext.Provider value={appSetupData}>
       <Layout title="Home | Next.js + TypeScript Example">
@@ -44,17 +53,23 @@ const AppSetupPage = ({ appConfig }: AppSetupPageProps) => {
         {appSetupData && (
           <div>
             <p>
-              Your app is setup, you can embed this in your Copilot dashboard using the following url:
+              Your app is setup! You can embed it as a Custom App in Copilot using the following url:
             </p>
             <p>
               {`https://mahalani.vercel.app/${appId}`}
             </p>
+            <p>
+              You may first want to test it out as a Manual App for just one of your clients.
+            </p>
+            <DataTable rows={myRows} />
           </div>
         )}
       </Layout>
     </AppContext.Provider>
   );
 };
+
+
 
 export async function getServerSideProps(context) {
   const session = await getServerSession(context.req, context.res, authOptions);
@@ -70,15 +85,33 @@ export async function getServerSideProps(context) {
 
   const { appId } = context.query
   let appConfig: AppContextType | null = null;
+  let clientData;
   try {
     appConfig = await fetchConfig(appId);
+
+    const copilotGetReq = {
+      headers: {
+        'X-API-KEY': appConfig.copilotApiKey,
+        'Content-Type': 'application/json',
+      },
+    };
+
+    const clientRes = await fetch(
+      `https://api.copilot-staging.com/v1/client`,
+      copilotGetReq,
+    );
+
+    clientData = (await clientRes.json()).data;
+    console.log(clientData);
+
   } catch (ex) {
     console.error('error fetching user apps', ex)
   }
 
   return {
     props: {
-      appConfig
+      appConfig,
+      clients: clientData
     },
   };
 }
