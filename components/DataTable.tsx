c77cc34cc0839a1a6f2592475aa0595cf49aa962
Potluck Mittal <potluck@copilot.com>
2023-02-02 11:25:22 +0800

    show individual URLs per client (#20)
    * show individual URLs per client
    
    * update column name

---
+++ components/DataTable.tsx
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import { DataGrid, GridColDef, GridValueGetterParams } from '@mui/x-data-grid';
+
+const columns: GridColDef[] = [
+  { field: 'clientName', headerName: 'Client name', width: 230 },
+  { field: 'url', headerName: 'Manual App URL', width: 630 },
+  { field: 'id', headerName: 'Client ID', width: 300 },
+];
+
+export default function DataTable(rows) {
+  return (
+    <div style={{ height: 400, width: '100%' }}>
+      <DataGrid
+        rows={rows.rows}
+        columns={columns}
+        pageSize={25}
+      />
+    </div>
+  );
+}
