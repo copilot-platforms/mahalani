03bf993e483fb7df87a922241adc489ff750249d
Neil Raina <mrneilraina@gmail.com>
2023-02-02 10:32:54 +0800

    update get started page design

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,10 @@
 import router from 'next/router'
 import { nanoid } from 'nanoid'
 import { useSession, signIn } from "next-auth/react"  
-
+import Image from 'next/image'
+import CopilotLogo from '../assets/Copilot_Icon_Circle.png';
 import Layout from '../components/Layout'
-import { Button } from '@mui/material';
+import { Button, Grid, Box, Typography } from '@mui/material';
 import { useEffect } from 'react';
 import { getServerSession } from 'next-auth';
 import { fetchUserApps } from './api/config/apiConfigUtils';
@@ -34,18 +35,32 @@ const IndexPage = () => {
 
   return (
     <Layout>
-      <h1>Client Task Management</h1>
-      <p>
-        This is an app for client task management that can be embedded in your Copilot dashboard.
-      </p>
-      {!session && (
-        <Button
-          onClick={() => signIn()}
-          color="primary"
-        >
-          Get Started
-        </Button>
-      )}
+      <Box display="flex" flexDirection="column" alignItems="center" mt={8}>
+        <Image
+            src={CopilotLogo}
+            alt="Copilot logo"
+            width="100"
+        />
+        <Box mt={4}>
+          <Typography variant="h2">Client Task Management</Typography>
+        </Box>
+        <Box mt={2}>
+          <Typography variant="h6">
+            This is an app for client task management that can be embedded in your Copilot dashboard.
+          </Typography>
+        </Box>
+        <Box mt={8}>
+          {!session && (
+            <Button
+              onClick={() => signIn()}
+              color="primary"
+              variant="contained"
+            >
+              Get Started
+            </Button>
+          )}
+        </Box>
+      </Box>
     </Layout>
   );
 }
