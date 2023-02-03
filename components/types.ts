ac4da7272a3286ee5ba24cbaa647a434ff5ce42b
Aymen <elhamzaouiaymen@gmail.com>
2023-02-03 15:48:50 +0800

    make rank optional

--- components/types.ts
+++ components/types.ts
@@ -20,7 +20,7 @@ export type Task = {
   status: TaskStatus;
   assignee?: AssigneeDataType;
   priority?: Priority;
-  rank: number;
+  rank?: number;
   description?: string;
   attachments?: AirTableAttachment[] | null;
   learnMoreLink?: string | null;
