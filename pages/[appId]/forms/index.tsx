1c54fc0e8ee5bbfdc9ff6860328cd2510a5c0a16
Allene Norton <norton.allene@gmail.com>
2023-11-29 14:57:25 -0500

    form fallback copy

--- pages/[appId]/forms/index.tsx
+++ pages/[appId]/forms/index.tsx
@@ -52,7 +52,7 @@ const Forms = () => {
 
   function handleRenderIframe() {
     const defaultEmptyPage = (
-      <EmptyPage title="Capital One" description="Appendix form is displayed to client based on their Collection tag." />
+      <EmptyPage title="Capital One" description="Appendix form is displayed to client based on their Premier or Lifestyle Collection tag." />
     );
 
     if (!clientCollection) {
